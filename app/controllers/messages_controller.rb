41d762b67faf42089ec87a4e39ba32353ff0e002
kanasakakei <soraito6254@yahoo.co.jp>
2020-06-13 13:57:08 +0900

    main-view create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,12 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
+    @message_all = Message.all
     @messages = @group.messages
   end
 
 
   def create
-    
     @message = @group.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
