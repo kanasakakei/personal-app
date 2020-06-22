56bb9e79056368f5ddfcaee4e638eeecec599e46
kanasakakei <soraito6254@yahoo.co.jp>
2020-06-22 17:03:59 +0900

    tab-redirect

---
+++ app/javascript/nav.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  $("#nav1").click(function() {
+    window.location.href = '/';
+    
+  });
+
+
+  $("#nav2").click(function() {
+    window.location.href = '/posts';
+    
+  });
+  
+
+
+});
