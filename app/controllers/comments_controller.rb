987b5b9b5e5d42aa166051516fc58b8456f63196
kanasakakei <soraito6254@yahoo.co.jp>
2020-06-22 19:14:20 +0900

    comment controller create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,26 @@ class CommentsController < ApplicationController
 def index
 end
 
+def create
+  @post = Post.find(params[:post_id])
+  @comment = @past.comments.create(comment_params)
+  
+end
+
+def destroy
+  @post = Post.find(params[:post_id])
+  @comment = @post.comments.find(params[:id])
+  @comment.destroy
+  redirect_to post_path(@post)
+end
+
+
+private
+
+def comment_params
+  params.require(:comment).permit(:content)
+  
+end
+
+
 end
