c6bafaece6d24b0164f904181d0f5bb67b391f60
kanasakakei <soraito6254@yahoo.co.jp>
2020-06-24 00:46:32 +0900

    post-edit create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,9 +27,15 @@ def update
   redirect_to post_path(post.id)
 end
 
+def destroy
+  post = Post.find(params[:id])
+  post.destroy
+  redirect_to root_path
+end
+
 private
 def post_params
-  params.require(:post).permit(:title, :content, :image)
+  params.require(:post).permit(:title, :content, :image).merge(user_od: current_user.id)
 end
 
 end
