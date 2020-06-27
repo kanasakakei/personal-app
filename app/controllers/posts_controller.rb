e82b520ab13bb13ad4102e2b970d6df38aedb7a8
kanasakakei <soraito6254@yahoo.co.jp>
2020-06-28 02:30:55 +0900

    bootstrap relation-fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
 
 def index
-  @posts = Post.all.order(created_at: 'DESC')
+  @posts = Post.all.order(created_at: 'DESC').page(params[:page]).per(10)
+
 end
 
 def new
