0d4c3fae47f2a5fea066f53fee09f61ee07c53a1
kanasakakei <soraito6254@yahoo.co.jp>
2020-07-03 22:43:13 +0900

    post-edit-view fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,50 +1,51 @@
 class PostsController < ApplicationController
 
-def index
-  @posts = Post.all.order(created_at: 'DESC').page(params[:page]).per(10)
-  @all_ranks = Post.find(Like.group(:post_id).order('count(post_id) DESC').limit(5).pluck(:post_id))
-end
-
-
-def new
-  @post = Post.new
-end
-
-def create
-  @post = Post.create(post_params)
-  @post.user_id = current_user.id
-  if @post.save
-    redirect_to posts_path
-  else
-    flash.now[:alert] = '【エラー】投稿できませんでした。'
-    redirect_to posts_path
+  def index
+    @posts = Post.all.order(created_at: 'DESC').page(params[:page]).per(10)
+    @all_ranks = Post.find(Like.group(:post_id).order('count(post_id) DESC').limit(5).pluck(:post_id))
   end
-end
 
-def show
-  @post = Post.find(params[:id])
-  @comment = Comment.new
-  @like = Like.new
-end
 
-def edit
-  @post = Post.find(params[:id])
-end
+  def new
+    @post = Post.new
+  end
 
-def update
-  post = Post.find(params[:id])
-  post.update(post_params)
-end
+  def create
+    @post = Post.create(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      flash.now[:alert] = '【エラー】投稿できませんでした。'
+      redirect_to posts_path
+    end
+  end
 
-def destroy
-  post = Post.find(params[:id])
-  post.destroy
-  redirect_to "/posts"
-end
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @like = Like.new
+  end
 
-private
-def post_params
-  params.require(:post).permit(:title, :content, :image).merge(user_id: current_user.id)
-end
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to "/posts"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :image).merge(user_id: current_user.id)
+  end
 
 end
