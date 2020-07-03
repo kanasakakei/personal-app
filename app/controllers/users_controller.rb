7544a36784620d45ce84122062798fe58d25a69c
kanasakakei <soraito6254@yahoo.co.jp>
2020-07-03 15:57:50 +0900

    user-edit fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
 
 def edit
+  @user = User.find(params[:id])
 end
 
 def new
@@ -15,8 +16,9 @@ def create
 end
 
 def update
+  @user = User.find(params[:id])
   if current_user.update(user_params)
-    redirect_to root_path
+    redirect_to user_path(@user)
   else
     render :edit
   end
@@ -30,7 +32,7 @@ end
 private
 
 def user_params
-  params.require(:user).permit(:name, :email, :image)
+  params.require(:user).permit(:name, :email, :image, :profile)
 end
 
 end
