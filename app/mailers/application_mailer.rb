6f149daf0b9e0754ff19fd19376234a1297b57e4
kanasakakei <soraito6254@yahoo.co.jp>
2020-07-13 19:47:09 +0900

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 module Mailers
   class ApplicationMailer < ActionMailer::Base
-    default from: "管理人 <from@example.com>"
-    layout "mailer"
+    default from: "管理人 <from@example.com>",
+    layout "mailer",
   end
 end
