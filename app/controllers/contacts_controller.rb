09c443c831d99bed053721b1c735a20c56f41b26
kanasakakei <soraito6254@yahoo.co.jp>
2020-07-13 19:34:01 +0900

    mailer fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,6 +16,13 @@ class ContactsController < ApplicationController
     end
   end
 
+  def update
+    contact = Contact.find(params[:id]) #contact_mailer.rbの引数を指定
+    contact.update(contact_params)
+    user = contact.user
+    ContactMailer.send_when_admin_reply(user, contact).deliver
+  end
+
   private
 
   def contact_params
