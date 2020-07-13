02150f894a857c5256aa197c08985fb465a69d35
kanasakakei <soraito6254@yahoo.co.jp>
2020-07-13 22:17:35 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module PersonalApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.autoloader = :classic
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
